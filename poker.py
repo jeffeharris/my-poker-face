f4c78adbca58b844327c37751b2590028219bd76
jeffeharris <jeffre4828@gmail.com>
2023-07-20 20:38:35 -0400

    updated main, same funciton

--- poker.py
+++ poker.py
@@ -528,11 +528,6 @@ class PokerGame:
 
 
 def main(test=False):
-    # Create Players for the game
-    definites = [
-        Player("Jeff")
-    ]
-    
     if test:
         basic_test_players = [
             Player("Player1"),
@@ -546,53 +541,9 @@ def main(test=False):
         game.set_dealer(players[1])
         
     else:
-        celebrities = [
-            AIPlayer("Ace Ventura", ai_temp=.9),
-            AIPlayer("Khloe and Kim Khardashian"),
-            AIPlayer("Fred Durst"),
-            AIPlayer("Tom Cruise"),
-            AIPlayer("James Bond"),
-            AIPlayer("Jon Stewart"),
-            AIPlayer("Jim Cramer", ai_temp=.7),
-            AIPlayer("Marjorie Taylor Greene", ai_temp=.7),
-            AIPlayer("Lizzo"),
-            AIPlayer("Bill Clinton"),
-            AIPlayer("Barack Obama"),
-            AIPlayer("Jesus Christ"),
-            AIPlayer("Triumph the Insult Dog", ai_temp=.7),
-            AIPlayer("Donald Trump", ai_temp=.7),
-            AIPlayer("Batman"),
-            AIPlayer("Deadpool"),
-            AIPlayer("Lance Armstrong"),
-            AIPlayer("A Mime", ai_temp=.8),
-            AIPlayer("Jay Gatsby"),
-            AIPlayer("Whoopi Goldberg"),
-            AIPlayer("Dave Chappelle"),
-            AIPlayer("Chris Rock"),
-            AIPlayer("Sarah Silverman"),
-            AIPlayer("Kathy Griffin"),
-            AIPlayer("Dr. Seuss", ai_temp=.7),
-            AIPlayer("Dr. Oz"),
-            AIPlayer("A guy who tells too many dad jokes")
-        ]
-
-        random.shuffle(celebrities)
-        randos = celebrities[0:(5-len(definites))]
-        players = definites + randos
-        for player in players:
-            if isinstance(player, AIPlayer):
-                i = random.randint(0, 2)
-                player.confidence = player.initialize_attribute("confidence", mood=i)
-                player.attitude = player.initialize_attribute("attittude", mood=i)
-        game = PokerGame(players)
-        game.set_dealer(players[random.randint(0, len(players) - 1)])
-
+        game = create_random_game()
     # Run the game until it ends
-    while len(game.players) > 1:
-        game.play_hand()
-        play_again = input("Play another hand? (y/n): ")
-        if play_again.lower() != "y":
-            break
+    game.run_game()
 
 
 def shift_list_left(my_list: list, count: int = 1):
@@ -613,7 +564,58 @@ def shift_list_right(my_list: list, count: int = 1):
     for i in range(1, count+1):
         # Pop from the end of the list and insert it at the beginning
         my_list.insert(0, my_list.pop())
-    
+
+
+def create_random_game(player_name: str = "Jeff", ai_players: [str] = None, num_players: int = 5):
+    # Create Players for the game
+    definites = [
+        Player(f"{player_name}")
+    ]
+
+    for ai_player in ai_players:
+        definites.append(AIPlayer(f"{ai_player}"))
+
+    celebrities = [
+        AIPlayer("Ace Ventura", ai_temp=.9),
+        AIPlayer("Khloe and Kim Khardashian"),
+        AIPlayer("Fred Durst"),
+        AIPlayer("Tom Cruise"),
+        AIPlayer("James Bond"),
+        AIPlayer("Jon Stewart"),
+        AIPlayer("Jim Cramer", ai_temp=.7),
+        AIPlayer("Marjorie Taylor Greene", ai_temp=.7),
+        AIPlayer("Lizzo"),
+        AIPlayer("Bill Clinton"),
+        AIPlayer("Barack Obama"),
+        AIPlayer("Jesus Christ"),
+        AIPlayer("Triumph the Insult Dog", ai_temp=.7),
+        AIPlayer("Donald Trump", ai_temp=.7),
+        AIPlayer("Batman"),
+        AIPlayer("Deadpool"),
+        AIPlayer("Lance Armstrong"),
+        AIPlayer("A Mime", ai_temp=.8),
+        AIPlayer("Jay Gatsby"),
+        AIPlayer("Whoopi Goldberg"),
+        AIPlayer("Dave Chappelle"),
+        AIPlayer("Chris Rock"),
+        AIPlayer("Sarah Silverman"),
+        AIPlayer("Kathy Griffin"),
+        AIPlayer("Dr. Seuss", ai_temp=.7),
+        AIPlayer("Dr. Oz"),
+        AIPlayer("A guy who tells too many dad jokes")
+    ]
+
+        random.shuffle(celebrities)
+        randos = celebrities[0:(5-len(definites))]
+        players = definites + randos
+        for player in players:
+            if isinstance(player, AIPlayer):
+                i = random.randint(0, 2)
+                player.confidence = player.initialize_attribute("confidence", mood=i)
+                player.attitude = player.initialize_attribute("attittude", mood=i)
+        game = PokerGame(players)
+        game.set_dealer(players[random.randint(0, len(players) - 1)])
+
 
 if __name__ == "__main__":
     main()
