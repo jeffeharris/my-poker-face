3ee012fcd87fba6f6976b72deab67ff015ebc613
jeffeharris <jeff.harris6@gmail.com>
2024-08-14 13:58:03 -0400

    Add dictionary serialization methods for Card and Deck
    Implemented to_dict and from_dict methods to allow Card and Deck objects to be converted to and from dictionaries. This improves the ability to easily serialize and deserialize these objects, facilitating storage and data transfer.

--- core/cards.py
+++ core/cards.py
@@ -36,13 +36,19 @@ class Card:
         return f"Card('{self.rank.ljust(2)}', '{self.suit}')"
 
     def __str__(self):
-        return f"{self.rank} of {self.suit}"
+        return f"{self.rank}{Card.SUIT_TO_ASCII[self.suit]}"
+
+    def __dict__(self):
+        return card_to_dict(self)
 
     def render_card(self):
         rank_left = self.rank.ljust(2)
         rank_right = self.rank.rjust(2)
         card = Card.CARD_TEMPLATE.format(rank_left, Card.SUIT_TO_ASCII[self.suit], Card.SUIT_TO_ASCII[self.suit], rank_right)
         return card
+
+    def to_dict(self):
+        return card_to_dict(self)
 
 
 class Deck:
@@ -55,6 +61,12 @@ class Deck:
         self.cards = [Card(rank, suit) for rank in Deck.RANKS for suit in Deck.SUITS]
         self.discard_pile = []
         self.shuffle()
+
+    def __dict__(self):
+        return deck_to_dict(self)
+
+    def to_dict(self):
+        return deck_to_dict(self)
 
     def shuffle(self) -> None:
         random.shuffle(self.cards)
@@ -104,3 +116,28 @@ def render_two_cards(card_1, card_2):
                                                           Card.SUIT_TO_ASCII[card_2.suit],
                                                           card_2.rank)
     return two_card_ascii_string
+
+
+def card_to_dict(card: Card) -> dict:
+    return {'rank': card.rank, 'suit': card.suit, 'value': card.value}
+
+
+def deck_to_dict(deck: Deck) -> dict:
+    deck_dict = {
+        'cards': [card_to_dict(card) for card in deck.cards],
+        'discard_pile': [card_to_dict(card) for card in deck.discard_pile]
+    }
+    return deck_dict
+
+def cards_from_dict(card_dict_list: List[dict]) -> List[Card]:
+    cards_list = []
+    for card_dict in card_dict_list:
+        card = Card(card_dict["rank"], card_dict["suit"])
+        cards_list.append(card)
+    return cards_list
+
+def deck_from_dict(deck_dict: dict) -> Deck:
+    deck = Deck()
+    deck.cards = cards_from_dict(deck_dict["cards"])
+    deck.discard_pile = cards_from_dict(deck_dict["discard_pile"])
+    return deck
