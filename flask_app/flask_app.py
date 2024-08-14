4a47359f95c0b864b9313d21a72b84cf26b795da
jeffeharris <jeff.harris6@gmail.com>
2024-08-14 13:55:33 -0400

    Implement basic poker game logic and player actions
    This commit adds initial poker game setup and player action handling in the Flask application. It introduces session management for game state, player actions processing, and updates the `/` and `/player_action` routes to support these features.

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -17,7 +17,50 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    pg= False
+    if 'game' not in session:
+        poker_players = get_players(test=False, num_players=2)
+        poker_game = PokerGame(poker_players, FlaskInterface())
+        session['game'] = poker_game.to_dict()
+
+    if not poker_game:
+        poker_game = poker_game_from_dict(poker_game_dict=session['game'])
+    poker_hand = PokerHand(interface=poker_game.interface,
+                           players=poker_game.players,
+                           dealer=poker_game.players[random.randint(0, len(poker_game.players) - 1)],
+                           deck=poker_game.deck)
+    poker_game.hands.append(poker_hand)
+    player_names = []
+    for player in poker_game.players:
+        player_names.append(player.name)
+    # FlaskInterface.display_game(poker_game)
+    return render_template(
+        template_name_or_list='index.html',
+        players=player_names
+    )
+
+
+@app.route('/player_action', methods=['POST'])
+def bet():
+    poker_game = session['game']
+    action = request.form['action']
+    amount = int(request.form['amount'])
+    poker_hand = poker_game.hands[-1]
+    hand_state = poker_hand.state
+    player_action = PokerAction(player=poker_game.players[0],
+                                action=action,
+                                amount=amount,
+                                hand_state=hand_state)
+    poker_hand.process_player_action(player=player_action.player,
+                                     player_action=player_action)
+    session['game'] = poker_game  # Save updated game state back to session
+    player_state = {}
+    for player in poker_game.players:
+        player_state[player.name] = player.money
+    return render_template(
+        'index.html',
+        players=player_state
+    )
 
 
 if __name__ == '__main__':
