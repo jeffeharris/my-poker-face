a6098c0116b86e333c1fa53c7f106b11341f894e
jeffeharris <jeff.harris6@gmail.com>
2024-08-15 10:27:09 -0400

    Refactor game interface and UI improvements
    Refactored the game interface to utilize Flask templates for dynamic content rendering. Updated the HTML structure, included session handling, and enhanced the UI with CSS to provide a better user experience. Added routes for game initiation and player actions.

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,6 +1,8 @@
 # flask_app/app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import random
+
+from core.cards import card_to_dict
 from core.game import Interface
 from core.poker import (PokerGame,
                         PokerAction,
@@ -13,8 +15,29 @@ from core.poker import (PokerGame,
 from dotenv import load_dotenv
 
 class FlaskInterface(Interface):
-    def display_game(self, game):
-        pass
+    @staticmethod
+    def display_game(g: PokerGame):
+        player_dict_list = []
+        for player in g.players:
+            player_dict_list.append(player.to_dict())
+
+        community_cards_dict_list = []
+        if len(g.hands) > 0:
+            for card in g.hands[-1].community_cards:
+                community_cards_dict_list.append(card_to_dict(card))
+
+        player_options = []
+        for player in g.remaining_players:
+            if isinstance(player, PokerPlayer):
+                player_options = player.options
+                break
+
+        return render_template(
+            template_name_or_list='html_poker_game.html',
+            players=player_dict_list,
+            community_cards=community_cards_dict_list ,
+            player_options=player_options
+        )
 
     def display_hand(self, hand):
         pass
@@ -31,7 +54,6 @@ class FlaskInterface(Interface):
     def display_poker_action(self, action):
         pass
 
-
 # def poker_game_from_dict(poker_game_dict: dict):
 #     players = player_list_from_dict(poker_game_dict["players"])
 #     interface = Interface.from_dict(poker_game_dict["interface"])
@@ -47,52 +69,104 @@ app = Flask(__name__)
 app.secret_key = 'my_secret_key_poker_app'
 
 
-@app.route('/')
+@app.route(rule='/', methods=['GET'])
 def home():
-    pg= False
-    if 'game' not in session:
-        poker_players = get_players(test=False, num_players=2)
-        poker_game = PokerGame(poker_players, FlaskInterface())
-        session['game'] = poker_game.to_dict()
+    return render_template(
+        template_name_or_list='index.html',
+    )
 
-    if not poker_game:
-        poker_game = poker_game_from_dict(poker_game_dict=session['game'])
+@app.route('/game', methods=['GET'])
+def game():
+    poker_players = get_players(test=False, num_players=2)
+    poker_game = PokerGame(poker_players, FlaskInterface())
+
     poker_hand = PokerHand(interface=poker_game.interface,
                            players=poker_game.players,
                            dealer=poker_game.players[random.randint(0, len(poker_game.players) - 1)],
                            deck=poker_game.deck)
     poker_game.hands.append(poker_hand)
-    player_names = []
-    for player in poker_game.players:
-        player_names.append(player.name)
-    # FlaskInterface.display_game(poker_game)
-    return render_template(
-        template_name_or_list='index.html',
-        players=player_names
-    )
+    player_queue = poker_hand.setup_hand()
+    poker_hand.betting_round(player_queue)
+    poker_hand.reveal_flop()
+
+    return poker_game.interface.display_game(g=poker_game)
+    # player_dict_list = []
+    # for player in poker_game.players:
+    #     player_dict_list.append(player.to_dict())
+    #
+    # community_cards_dict_list = []
+    # for card in poker_game.hands[-1].community_cards:
+    #     community_cards_dict_list.append(card_to_dict(card))
+    #
+    # return render_template(
+    #     template_name_or_list='html_poker_game.html',
+    #     players=player_dict_list,
+    #     community_cards=community_cards_dict_list
+    # )
 
 
-@app.route('/player_action', methods=['POST'])
-def bet():
+# @app.route('/api/start_game', methods=['GET'])
+# def start_game():
+#     poker_players = get_players(test=False, num_players=2)
+#     poker_game = PokerGame(poker_players, FlaskInterface())
+#     # if 'game' not in session:
+#     #     poker_players = get_players(test=False, num_players=2)
+#     #     poker_game = PokerGame(poker_players, FlaskInterface())
+#     #     session['game'] = poker_game.to_dict()
+#     #
+#     # if not poker_game:
+#     #     # poker_game = poker_game_from_dict(poker_game_dict=session['game'])
+#     #     pass
+#
+#     poker_game.interface.display_game(game=poker_game)
+#
+#     poker_hand = PokerHand(interface=poker_game.interface,
+#                            players=poker_game.players,
+#                            dealer=poker_game.players[random.randint(0, len(poker_game.players) - 1)],
+#                            deck=poker_game.deck)
+#     poker_game.hands.append(poker_hand)
+#
+#     poker_game.remaining_players, session['dealer'] = poker_hand.play_hand()
+#
+#     player_dict_list = []
+#     for player in poker_game.players:
+#         player_dict_list.append(player.to_dict())
+#
+#     community_cards_dict_list = []
+#     for card in poker_game.hands[-1].community_cards:
+#         community_cards_dict_list.append(card_to_dict(card))
+#
+#     player_options = []
+#     for player in poker_game.remaining_players:
+#         if isinstance(player, PokerPlayer):
+#             player_options = player.options
+#             break
+#
+#     return render_template(
+#         template_name_or_list='html_poker_game.html',
+#         players=player_dict_list,
+#         community_cards=poker_game.hands[-1].community_cards,
+#         player_options=player_options
+#     )
+
+
+@app.route('/api/player_action', methods=['POST'])
+def player_action():
+    data = request.get_json()
+    action = data.get('action')
     poker_game = session['game']
-    action = request.form['action']
     amount = int(request.form['amount'])
     poker_hand = poker_game.hands[-1]
     hand_state = poker_hand.state
-    player_action = PokerAction(player=poker_game.players[0],
-                                action=action,
-                                amount=amount,
-                                hand_state=hand_state)
-    poker_hand.process_player_action(player=player_action.player,
-                                     player_action=player_action)
+    players_poker_action = PokerAction(player=poker_game.players[0],   # TODO: replace with correct player logic
+                                        action=action,
+                                        amount=amount,
+                                        hand_state=hand_state)
+    poker_hand.process_player_action(player=players_poker_action.player,
+                                     player_action=players_poker_action)
     session['game'] = poker_game  # Save updated game state back to session
-    player_state = {}
-    for player in poker_game.players:
-        player_state[player.name] = player.money
-    return render_template(
-        'index.html',
-        players=player_state
-    )
+
+    return poker_game.interface.display_game(g=poker_game)       # TODO: update this to not have to pass the game
 
 
 if __name__ == '__main__':
