86910529fb1f5f3257ef689c5db536851a07c3bd
jeffeharris <jeff.harris6@gmail.com>
2024-08-21 01:36:19 -0400

    Add SocketIO for real-time updates and refactor game state management
    Implemented SocketIO to support real-time updates and improve user interaction during the poker game. Refactored game state management by introducing a new session-based approach, added functions to initialize and process game state, and updated route and event handlers accordingly.

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,16 +1,17 @@
-# flask_app/app.py
+# flask_app/flask_app.py
 from flask import Flask, render_template, session, request, redirect, jsonify
+from flask_socketio import SocketIO, emit
+from flask_session import Session
+
 import random
 
-from core.cards import card_to_dict
 from core.game import Interface
 from core.poker import (PokerGame,
                         PokerAction,
-                        PokerHand,
-                        PokerPlayer,
-                        AIPokerPlayer,
-                        get_players,
-                        shift_list_left)
+                        PokerHand)
+from core.poker_player import PokerPlayer, AIPokerPlayer
+from core.utils import get_players, shift_list_left
+from core.serialization import obj_to_dict
 
 from dotenv import load_dotenv
 
@@ -24,7 +25,7 @@ class FlaskInterface(Interface):
         community_cards_dict_list = []
         if len(g.hands) > 0:
             for card in g.hands[-1].community_cards:
-                community_cards_dict_list.append(card_to_dict(card))
+                community_cards_dict_list.append(card.to_dict())
 
         player_options = []
         for player in g.remaining_players:      # TODO: update this to show all players and indicate if in hand still
@@ -54,26 +55,17 @@ class FlaskInterface(Interface):
     def display_poker_action(self, action):
         pass
 
-# def poker_game_from_dict(poker_game_dict: dict):
-#     players = player_list_from_dict(poker_game_dict["players"])
-#     interface = Interface.from_dict(poker_game_dict["interface"])
-#
-#     poker_game = PokerGame(players, interface)
-#     poker_game.starting_players = player_list_from_dict(poker_game_dict["starting_players"])
-#     poker_game.remaining_players = player_list_from_dict(poker_game_dict["remaining_players"])
-#     poker_game.deck = deck_from_dict(poker_game_dict["deck"])
-#     poker_game.hands = hand_list_from_dict(poker_game_dict["hands"])
-#     return poker_game
-
 app = Flask(__name__,
             template_folder='./templates',
             static_folder='./static')
-
-app.secret_key = 'my_secret_key_poker_app'
+app.config['SECRET_KEY'] = 'my_secret_key_poker_app'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+socketio = SocketIO(app, manage_session=False)
 
 @app.route(rule='/', methods=['GET'])
 def index():
-    if "game" not in session:
+    if "game_state" not in session:
         return redirect(
             location='/home',
             code=302
@@ -87,92 +79,45 @@ def index():
 @app.route(rule='/home', methods=['GET'])
 def home():
     return render_template(
-        template_name_or_list='index.html',
+        template_name_or_list='home.html',
     )
 
 @app.route('/game', methods=['GET'])
 def game():
-    poker_players = get_players(test=False, num_players=2)
-    poker_game = PokerGame(poker_players, FlaskInterface())
+    return render_template(
+        template_name_or_list='poker_game.html',
+    )
 
-    poker_hand = PokerHand(interface=poker_game.interface,
-                           players=poker_game.players,
-                           dealer=poker_game.players[random.randint(0, len(poker_game.players) - 1)],
-                           deck=poker_game.deck)
-    poker_game.hands.append(poker_hand)
-    player_queue = poker_hand.setup_hand()
-    poker_hand.betting_round(player_queue)
-    poker_hand.reveal_flop()
-
-    return poker_game.interface.display_game(g=poker_game)
-
-# @app.route('/api/start_game', methods=['POST'])
-# def start_game():
-#     poker_players = get_players(test=False, num_players=2)
-#     poker_game = PokerGame(poker_players, FlaskInterface())
-#     # if 'game' not in session:
-#     #     poker_players = get_players(test=False, num_players=2)
-#     #     poker_game = PokerGame(poker_players, FlaskInterface())
-#     #     session['game'] = poker_game.to_dict()
-#     #
-#     # if not poker_game:
-#     #     # poker_game = poker_game_from_dict(poker_game_dict=session['game'])
-#     #     pass
-#
-#     poker_game.interface.display_game(game=poker_game)
-#
-#     poker_hand = PokerHand(interface=poker_game.interface,
-#                            players=poker_game.players,
-#                            dealer=poker_game.players[random.randint(0, len(poker_game.players) - 1)],
-#                            deck=poker_game.deck)
-#     poker_game.hands.append(poker_hand)
-#
-#     poker_game.remaining_players, session['dealer'] = poker_hand.play_hand()
-#
-#     player_dict_list = []
-#     for player in poker_game.players:
-#         player_dict_list.append(player.to_dict())
-#
-#     community_cards_dict_list = []
-#     for card in poker_game.hands[-1].community_cards:
-#         community_cards_dict_list.append(card_to_dict(card))
-#
-#     player_options = []
-#     for player in poker_game.remaining_players:
-#         if isinstance(player, PokerPlayer):
-#             player_options = player.options
-#             break
-#
-#     return render_template(
-#         template_name_or_list='html_poker_game.html',
-#         players=player_dict_list,
-#         community_cards=poker_game.hands[-1].community_cards,
-#         player_options=player_options
-#     )
+@app.route('/start-game', methods=['POST'])
+def start_game():
+    if "game_state" in session:
+        game_state = session.get('game_state')
+    else:
+        game_state = initialize_game_state()
+    session['game_state'] = game_state
+    socketio.emit('update_game_state', session['game_state'])
+    return jsonify(session['game_state'])
 
 
-@app.route('/api/player_action', methods=['POST'])
-def player_action():
-    data = request.get_json()
+@socketio.on('player_action')
+def handle_player_action(data):
+    game_state = session.get('game_state', initialize_game_state())
+    # data = request.get_json()
     action = data.get('action')
-    poker_game = session['game']
-    amount = int(request.form['amount'])
-    poker_hand = poker_game.hands[-1]
-    hand_state = poker_hand.state
-    players_poker_action = PokerAction(player=poker_game.players[0],   # TODO: replace with correct player logic
-                                        action=action,
-                                        amount=amount,
-                                        hand_state=hand_state)
-    poker_hand.process_player_action(player=players_poker_action.player,
-                                     player_action=players_poker_action)
-    session['game'] = poker_game  # Save updated game state back to session
 
-    return poker_game.interface.display_game(g=poker_game)       # TODO: update this to not have to pass the game
+    if game_state["current_player"]["type"] == "PokerPlayer":
+        game_state = process_player_action(game_state, action)
+        session['game_state'] = game_state
+        socketio.emit('update_game_state', game_state)
+        if game_state["current_player"]["type"] == "AIPokerPlayer":
+            handle_ai_turns()
 
 
+# TODO: update these messages interactions to use socketio for real time back/forth
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    return jsonify(game['messages'])
+    return {'message 1': 'this is the message'}
+    # return jsonify(game['messages'])
 
 
 @app.route('/messages', methods=['POST'])
@@ -182,6 +127,30 @@ def add_message():
         game['messages'].append(new_message)
         return jsonify({"status": "success"}), 201
     return jsonify({"status": "error"}), 400
+
+
+def initialize_game_state():
+    poker_players = get_players(test=False, num_players=2)
+    poker_game = PokerGame(poker_players, FlaskInterface())
+
+    poker_hand = PokerHand(interface=poker_game.interface,
+                           players=poker_game.players,
+                           dealer=poker_game.players[random.randint(0, len(poker_game.players) - 1)],
+                           deck=poker_game.deck)
+    poker_game.hands.append(poker_hand)
+    poker_hand.setup_hand()
+    return jsonify(obj_to_dict(poker_game))
+
+
+def process_player_action(game_state, action):
+    # get poker_hand
+    poker_hand_dict = game_state['hands'][-1]
+    poker_hand = PokerHand.from_dict(poker_hand_dict, FlaskInterface())
+    player_action = PokerAction.from_dict(action)
+    poker_hand.process_player_action(player=game_state['current_player'],
+                                     poker_action=player_action)
+    game_state['hands'][-1] = poker_hand.to_dict()
+    return game_state
 
 
 if __name__ == '__main__':
