b4f6f2d1ecb66031cd7fe2fcce26717be12be5cd
jeffeharris <jeff.harris6@gmail.com>
2024-08-29 22:03:16 -0400

    Refactor `Deck` class and introduce `CardSet` class
    Renamed inner `Deck` mechanisms and encapsulated card operations in a new `CardSet` class. This change enhances modularity and clarity, allowing for better handling of card sets and simplifying deck management operations. Needs more work.

--- core/deck.py
+++ core/deck.py
@@ -2,21 +2,48 @@ import random
 from core.card import *
 
 
-class Deck:
+class CardSet:
     RANKS = list(Card.RANK_VALUES.keys())
     SUITS = list(Card.SUIT_TO_ASCII.keys())
     cards: List[Card]
-    discarded_cards: List[Card]
 
     def __init__(self):
-        self.cards = self._init_cards()
-        self.discard_pile = []
-        self.shuffle()
+        self.cards = []
 
-    @staticmethod
-    def _init_cards() -> List[Card]:
-        """Initialize the deck with cards."""
-        return [Card(rank, suit) for rank in Deck.RANKS for suit in Deck.SUITS]
+    def __len__(self):
+        return len(self.cards)
+
+    def __getitem__(self, index):
+        return self.cards[index]
+
+    @classmethod
+    def cards_from_dict(cls, deck_dict: Dict[str, List[Dict[str, str]]]) -> 'CardSet':
+        """Create a Deck instance from a dictionary representation."""
+        cards = cls()
+        cards.cards = Card.list_from_dict_list(deck_dict['cards'])
+        cards.discard_pile = Card.list_from_dict_list(deck_dict['discard_pile'])
+        return cards
+
+    def deal(self, deal_to: 'CardSet', num: int = 1) -> None:
+        """Deal a number of cards from the Deck."""
+        cards = [self.cards.pop() for _ in range(num)]
+        deal_to._add_cards(cards)
+        return None
+
+    def _add_cards(self, cards):
+        for card in cards:
+            self.cards.append(card)
+
+
+class Deck:
+    card_deck: CardSet
+    discard_pile: CardSet
+
+    def __init__(self):
+        super().__init__()
+        self.cards = self._init_deck()
+        self.discard_pile = CardSet()
+        self.shuffle()
 
     def to_dict(self) -> Dict[str, List[Dict[str, str]]]:
         """Convert the Deck to a dictionary representation."""
@@ -26,31 +53,25 @@ class Deck:
         }
         return deck_dict
 
-    @classmethod
-    def from_dict(cls, deck_dict: Dict[str, List[Dict[str, str]]]) -> 'Deck':
-        """Create a Deck instance from a dictionary representation."""
-        deck = cls()
-        deck.cards = Card.list_from_dict_list(deck_dict['cards'])
-        deck.discard_pile = Card.list_from_dict_list(deck_dict['discard_pile'])
-        return deck
+    @staticmethod
+    def _init_deck() -> List[Card]:
+        """Initialize the deck with cards."""
+        cs = CardSet()
+        cs._add_cards([Card(rank, suit) for rank in Deck.RANKS for suit in Deck.SUITS])
+        return cs.cards
 
     def shuffle(self) -> None:
         """Shuffle the deck of cards."""
         random.shuffle(self.cards)
 
-    def deal(self, num=1) -> List[Card]:
-        """Deal a number of cards from the Deck."""
-        return [self.cards.pop() for _ in range(num)]
-
-    def discard(self, num=1) -> List[Card]:
+    def discard(self, num=1) -> None:
         """Discard a number of cards from the Deck."""
-        discarded_cards = self.deal(num)
-        self.discard_pile += discarded_cards
-        return discarded_cards
+        self.deal(self.discard_pile, num)
+        return None
 
     def _return_cards_to_deck(self, cards: List[Card], shuffle: bool = True) -> None:
         """Return cards to the Deck and optionally shuffle."""
-        self.cards += cards
+        self.cards._add_cards
         if shuffle:
             self.shuffle()
 
